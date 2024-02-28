d97bd89a68e6cf898985e553f148b6f1a831927d
Nikolaj <2525536+svc-user@users.noreply.github.com>
2024-02-28 15:48:38 +0100

    IT WERKS!

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,14 @@
 const std = @import("std");
 const windows = std.os.windows;
 const user32 = @import("user32ext.zig");
-const winmm = @import("winmm.zig");
-
-// const soundio = @cImport({
-//     @cInclude("C:/Users/nsc/code/CLACK/src/vendor/libsoundio-1.1.0/soundio/soundio.h");
-// });
+//const winmm = @import("winmm.zig");
+const soundio = @import("soundio");
 
 const State = struct {
-    wavHandl: winmm.HWAVEOUT = undefined,
+    // wavHandl: winmm.HWAVEOUT = undefined,
     hhk: user32.HHOOK = undefined,
     canPlay: bool = false,
+    currentBuffer: [1024]f32 = undefined,
 };
 var state: State = .{};
 
@@ -19,33 +17,43 @@ var keyMap = std.AutoHashMap(Keys, []const u8).init(allocator);
 var keyMapShift = std.AutoHashMap(Keys, []const u8).init(allocator);
 
 pub fn main() !void {
-    //const sio = soundio.soundio_create();
-    //_ = sio;
+    var sio = try soundio.SoundIo.init();
+    defer sio.deinit();
+
+    var outstream = try sio.createOutputStream(allocator, .{
+        .channel_layout = .mono,
+        .sample_rate = 22050,
+        .write_callback = sndio_callback,
+    });
+    defer outstream.deinit();
+
+    try outstream.start();
+
     try addKeyMap();
     try addKeyMapShift();
     defer keyMap.deinit();
     defer keyMapShift.deinit();
 
-    var hndl: windows.LONG_PTR = @as(windows.LONG_PTR, 0);
-    var pwfx: winmm.WAVEFORMATEX = .{
-        .wFormatTag = winmm.WAVE_FORMAT_PCM,
-        .nChannels = 1,
-        .nSamplesPerSec = 22050,
-        .nAvgBytesPerSec = 176000,
-        .nBlockAlign = (1 * 8) / 8,
-        .wBitsPerSample = 8,
-        .cbSize = 0,
-    };
-    var lppwfx: winmm.LPCWAVEFORMATEX = &pwfx;
+    // var hndl: windows.LONG_PTR = @as(windows.LONG_PTR, 0);
+    // var pwfx: winmm.WAVEFORMATEX = .{
+    //     .wFormatTag = winmm.WAVE_FORMAT_PCM,
+    //     .nChannels = 1,
+    //     .nSamplesPerSec = 22050,
+    //     .nAvgBytesPerSec = 176000,
+    //     .nBlockAlign = (1 * 8) / 8,
+    //     .wBitsPerSample = 8,
+    //     .cbSize = 0,
+    // };
+    // var lppwfx: winmm.LPCWAVEFORMATEX = &pwfx;
 
-    var mmr = winmm.waveOutOpen(&hndl, winmm.MM_WAVE_MAPPER, lppwfx, &waveCallback, 0, winmm.CALLBACK_FUNCTION | winmm.WAVE_FORMAT_DIRECT);
-    if (mmr != winmm.MMSYSERR_NOERROR) {
-        std.debug.print("Unable to open audio device. err: {d}\n", .{mmr});
-        return;
-    }
+    // var mmr = winmm.waveOutOpen(&hndl, winmm.MM_WAVE_MAPPER, lppwfx, &waveCallback, 0, winmm.CALLBACK_FUNCTION | winmm.WAVE_FORMAT_DIRECT);
+    // if (mmr != winmm.MMSYSERR_NOERROR) {
+    //     std.debug.print("Unable to open audio device. err: {d}\n", .{mmr});
+    //     return;
+    // }
 
-    const uhndl: usize = @intCast(hndl);
-    state.wavHandl = @ptrFromInt(uhndl);
+    // const uhndl: usize = @intCast(hndl);
+    // state.wavHandl = @ptrFromInt(uhndl);
     defer {
         std.debug.print("Unhooking.\n", .{});
         var uhr: u8 = 0;
@@ -55,13 +63,13 @@ pub fn main() !void {
             std.time.sleep(0.5 * 1000 * 1000 * 1000);
         }
 
-        std.debug.print("Stopping audio output.\n", .{});
-        mmr = 1;
-        while (mmr != 0) {
-            mmr = winmm.waveOutClose(state.wavHandl);
-            std.debug.print("waveOutClose returned {any}.\n", .{mmr});
-            std.time.sleep(0.5 * 1000 * 1000 * 1000);
-        }
+        // std.debug.print("Stopping audio output.\n", .{});
+        // mmr = 1;
+        // while (mmr != 0) {
+        //     mmr = winmm.waveOutClose(state.wavHandl);
+        //     std.debug.print("waveOutClose returned {any}.\n", .{mmr});
+        //     std.time.sleep(0.5 * 1000 * 1000 * 1000);
+        // }
     }
 
     const nto = windows.kernel32.CreateThread(null, 0, &messageListener, null, 0, null);
@@ -88,6 +96,34 @@ pub fn main() !void {
     }
 }
 
+var phase: f32 = 0.0;
+fn sndio_callback(arg: ?*anyopaque, num_frames: usize, buffer: *soundio.Buffer) void {
+    // _ = arg;
+
+    // var frame: usize = 0;
+    // while (frame < num_frames) : (frame += 1) {
+    //     buffer.channels[0].data[frame] = state.currentBuffer[frame];
+    // }
+
+    // state.canPlay = true;
+
+    _ = arg;
+
+    const freq: f32 = 261.63; // Middle C.
+    const sample_rate_f: f32 = 22050;
+    const amplitude: f32 = 0.4; // Not too loud.
+
+    var frame: usize = 0;
+    while (frame < num_frames) : (frame += 1) {
+        const val = amplitude * (2.0 * std.math.fabs(2.0 * phase - 1.0) - 1);
+        buffer.channels[0].set(frame, val);
+
+        phase += freq / sample_rate_f;
+        if (phase >= 1.0)
+            phase -= 1.0;
+    }
+}
+
 fn messageListener(_: windows.LPVOID) callconv(windows.WINAPI) windows.DWORD {
     std.debug.print("Setting hook.\n", .{});
 
@@ -98,6 +134,8 @@ fn messageListener(_: windows.LPVOID) callconv(windows.WINAPI) windows.DWORD {
     if (@intFromPtr(state.hhk) != 0) {
         std.debug.print("Waiting. Hook is {any}\n", .{state.hhk});
     }
+
+    state.canPlay = true;
 
     while (true) {
         var msg: windows.user32.MSG = undefined;
@@ -142,31 +180,31 @@ pub fn hookHandler(code: windows.INT, wParam: windows.WPARAM, lParam: windows.LP
     return user32.CallNextHookEx(0, code, wParam, lParam);
 }
 
-fn waveCallback(hwo: winmm.HWAVEOUT, uMsg: windows.UINT, dwInst: windows.DWORD_PTR, dwParam1: windows.DWORD_PTR, dwParam2: windows.DWORD_PTR) void {
-    _ = hwo;
-    _ = dwInst;
-    _ = dwParam1;
-    _ = dwParam2;
+// fn waveCallback(hwo: winmm.HWAVEOUT, uMsg: windows.UINT, dwInst: windows.DWORD_PTR, dwParam1: windows.DWORD_PTR, dwParam2: windows.DWORD_PTR) void {
+//     _ = hwo;
+//     _ = dwInst;
+//     _ = dwParam1;
+//     _ = dwParam2;
 
-    const msg = switch (uMsg) {
-        winmm.WOM_OPEN => "WOM_OPEN",
-        winmm.WOM_CLOSE => "WOM_CLOSE",
-        winmm.WOM_DONE => "WOM_DONE",
-        else => {
-            std.debug.print("{d}\n", .{uMsg});
-            @panic("unknown msg type.");
-        },
-    };
+//     const msg = switch (uMsg) {
+//         winmm.WOM_OPEN => "WOM_OPEN",
+//         winmm.WOM_CLOSE => "WOM_CLOSE",
+//         winmm.WOM_DONE => "WOM_DONE",
+//         else => {
+//             std.debug.print("{d}\n", .{uMsg});
+//             @panic("unknown msg type.");
+//         },
+//     };
 
-    switch (uMsg) {
-        winmm.WOM_OPEN => state.canPlay = true,
-        winmm.WOM_CLOSE => state.canPlay = false,
-        winmm.WOM_DONE => state.canPlay = true,
-        else => unreachable, // would've paniced before reaching this
-    }
+//     switch (uMsg) {
+//         winmm.WOM_OPEN => state.canPlay = true,
+//         winmm.WOM_CLOSE => state.canPlay = false,
+//         winmm.WOM_DONE => state.canPlay = true,
+//         else => unreachable, // would've paniced before reaching this
+//     }
 
-    std.debug.print("uMsg: {s}\n", .{msg});
-}
+//     std.debug.print("uMsg: {s}\n", .{msg});
+// }
 
 fn playClack(sType: Sound, _: KeyState) void {
     if (!state.canPlay) return;
@@ -184,29 +222,45 @@ fn playClack(sType: Sound, _: KeyState) void {
         .Generic3 => @embedFile("sounds/generic3.wav"),
         .Generic4 => @embedFile("sounds/generic4.wav"),
     };
-
-    var wh: winmm.WAVEHDR = .{
-        .lpData = @ptrCast(@constCast(soundFile)),
-        .dwBufferLength = @intCast(soundFile.len),
-        .dwBytesRecorded = 0,
-        .dwUser = 0,
-        .dwFlags = 0,
-        .dwLoops = 0,
-        .lpNext = 0,
-        .reserved = 0,
-    };
-
-    var pwh: winmm.LPWAVEHDR = &wh;
-    var mmr = winmm.waveOutPrepareHeader(state.wavHandl, pwh, @sizeOf(winmm.WAVEHDR));
-    if (mmr != winmm.MMSYSERR_NOERROR) return;
-
-    std.debug.print("waveOutPrepareHeader.dwFlags: {b:08}\n", .{wh.dwFlags});
-
-    mmr = winmm.waveOutWrite(state.wavHandl, pwh, @sizeOf(winmm.WAVEHDR));
-    if (mmr != winmm.MMSYSERR_NOERROR) return;
     state.canPlay = false;
 
-    std.debug.print("waveOutWrite.dwFlags:         {b:08}\n", .{wh.dwFlags});
+    var bi: usize = 0;
+    var i: usize = 0;
+    while (i < soundFile.len) : (i += 4) {
+        const b1: u32 = if (i + 0 < soundFile.len) @shlExact(@as(u32, soundFile[i + 0]), 24) else 0;
+        const b2: u32 = if (i + 1 < soundFile.len) @shlExact(@as(u32, soundFile[i + 1]), 16) else 0;
+        const b3: u32 = if (i + 2 < soundFile.len) @shlExact(@as(u32, soundFile[i + 2]), 8) else 0;
+        const b4: u32 = if (i + 3 < soundFile.len) @shlExact(@as(u32, soundFile[i + 3]), 0) else 0;
+
+        const f: f32 = @bitCast(b1 | b2 | b3 | b4);
+        state.currentBuffer[bi] = f;
+        bi += 1;
+    }
+    while (bi < state.currentBuffer.len) : (bi += 1) {
+        state.currentBuffer[bi] = 0;
+    }
+
+    // var wh: winmm.WAVEHDR = .{
+    //     .lpData = @ptrCast(@constCast(soundFile)),
+    //     .dwBufferLength = @intCast(soundFile.len),
+    //     .dwBytesRecorded = 0,
+    //     .dwUser = 0,
+    //     .dwFlags = 0,
+    //     .dwLoops = 0,
+    //     .lpNext = 0,
+    //     .reserved = 0,
+    // };
+
+    // var pwh: winmm.LPWAVEHDR = &wh;
+    // var mmr = winmm.waveOutPrepareHeader(state.wavHandl, pwh, @sizeOf(winmm.WAVEHDR));
+    // if (mmr != winmm.MMSYSERR_NOERROR) return;
+
+    // std.debug.print("waveOutPrepareHeader.dwFlags: {b:08}\n", .{wh.dwFlags});
+
+    // mmr = winmm.waveOutWrite(state.wavHandl, pwh, @sizeOf(winmm.WAVEHDR));
+    // if (mmr != winmm.MMSYSERR_NOERROR) return;
+
+    // std.debug.print("waveOutWrite.dwFlags:         {b:08}\n", .{wh.dwFlags});
 
     //std.time.sleep(1 * 1000 * 1000 * 1000);
 
